fef06f57b4d469fed74aa116a7020a597cd8c59c
Foreen03 <128678869+Foreen03@users.noreply.github.com>
2025-04-29 12:17:16 +0800

    Applied Singleton on Flask_App

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 
-class Flask_App():
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class Flask_App(metaclass = Singleton):
     
     def __init__(self, app: Flask, db, bcrypt, session) -> None:
         self._app = app
