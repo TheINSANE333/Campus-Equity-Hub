83fcc4e99529d690ff082db7ea16856b2ab32799
Foreen03 <128678869+Foreen03@users.noreply.github.com>
2025-06-10 15:49:38 +0800

    Enhance user dashboard

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, flash, session, redirect
 from app.app_stub import Flask_App_Stub
 from app.routes.endpoint import Endpoint
+from app.item_dbhandler import ItemRepository
 
 class Dashboard(Endpoint):
     def __init__(self, app: Flask_App_Stub) -> None:
@@ -16,4 +17,12 @@ class Dashboard(Endpoint):
             flash('Please log in to access this page.', 'danger')
             return redirect(url_for('login'))
         
-        return render_template('dashboard.html', username=session['username'])
+        item_dbHandler = ItemRepository(self.flask_app)
+        items = item_dbHandler.get_available_items()
+
+        context = {
+            'username': session['username'],
+            'items': items
+        }
+        
+        return render_template('dashboard.html', **context)
